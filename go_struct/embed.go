3be12b0ad56d8f47310f488c0bee22999f0251c0
EZURA, Atsushi <zurazurataicho@gmail.com>
2022-01-29 16:07:47 +0900

    add literal definition

--- go_struct/embed.go
+++ go_struct/embed.go
@@ -16,6 +16,10 @@ func embed() {
 	c.Engine.Type = "M15A-FKS"
 	c.Engine.Rpm = 2000
 	c.Wheel.Degree = 20
-	fmt.Println("> embedded structure: c.Maker = ...;")
-	fmt.Printf("c = %v\n\n", c)
+
+	ec := EmbedCar{"Honda", Wheel{0}, Engine{"B20B", 2500}}
+
+	fmt.Println("> embedded structure: c.Maker = ...; ec := EmbedCar{...}")
+	fmt.Printf("c = %v\n", c)
+	fmt.Printf("ec = %v\n\n", ec)
 }
